3acb9f6ddee220acc03aa19767178f7172f19ba0
raptorVladimirAdigeya <98263225+raptorVladimirAdigeya@users.noreply.github.com>
2022-02-13 17:52:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.main__slider', { 
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop: true,
+  
+ 
+    navigation: {
+      nextEl: '.main__slider-arrow',
+    },
+  
+
+  });

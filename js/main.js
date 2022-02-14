2b91b32b1fabf4e792fe896ceafdbfb319fea678
raptorVladimirAdigeya <98263225+raptorVladimirAdigeya@users.noreply.github.com>
2022-02-14 19:40:40 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,44 @@
 const swiper = new Swiper('.main__slider', { 
-    slidesPerView: 2,
+    
     spaceBetween: 30,
-    loop: true,
-  
+    loop: true, 
  
     navigation: {
       nextEl: '.main__slider-arrow',
     },
-  
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      }
+    }
 
   });
+let burger = document.querySelector('.menu-burger');
+let closeMenu = document.querySelector('.header__menu-close ');
+let menuMobule = document.querySelector('.header');
+const modalPlay = document.querySelector('.main__button-play');
+const modal = document.querySelector('.modal')
+
+burger.addEventListener('click', () => {
+  menuMobule.style.display = 'block'
+})
+
+closeMenu.addEventListener('click', () => {
+  menuMobule.style.display = 'none'
+})
+
+modalPlay.addEventListener('click', function() {
+  modal.classList.remove('close')
+})
+modal.addEventListener('click', function(){
+  modal.classList.add('close')
+})
+modal.querySelector('.modal-inner').addEventListener('click', function (event){
+  
+  event.stopPropagation()
+})
+   
+    
